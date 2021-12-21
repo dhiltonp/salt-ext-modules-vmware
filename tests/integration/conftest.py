c2236b4555c53798cc58b515998414fd5d996b4f
David Hilton <david.hilton.p@gmail.com>
2021-12-21 13:43:55 -0700

    allow pytest to run from other directories by looking for vcenter.conf relative to conftest.py

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,10 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 import json
 import os
-import ssl
 import uuid
 from collections import namedtuple
-from configparser import ConfigParser
 from pathlib import Path
 
 import pytest
@@ -54,7 +52,7 @@ def salt_call_cli(minion):
 
 @pytest.fixture(scope="session")
 def integration_test_config():
-    default_path = Path().parent.parent / "local" / "vcenter.conf"
+    default_path = Path(__file__).parent.parent.parent / "local" / "vcenter.conf"
     config_path = Path(os.environ.get("VCENTER_CONFIG", default_path))
 
     try:
